5edde24d1d905317e72d8000fdd917b5c8cc97a6
Safa Yilmaz <safa.yilmaz@yahoo.com>
2021-10-04 22:00:50 +0300

    multiple loggers

--- Interface/src/Interface/CustomerManager.java
+++ Interface/src/Interface/CustomerManager.java
@@ -1,11 +1,10 @@
 package Interface;
 
 public class CustomerManager {
-    private Logger logger;
-    public CustomerManager(Logger logger) {
-        this.logger = logger;
+    private Logger[] loggers;
+    public CustomerManager(Logger[] loggers) {
+        this.loggers = loggers;
     }
-
 
     public void Add(Customer customer){
         System.out.println(customer.getFirstName() + " " + customer.getLastName() + " the customer has been added to DB");
--- Interface/src/Interface/Main.java
+++ Interface/src/Interface/Main.java
@@ -1,17 +1,18 @@
 package Interface;
 
 import javax.xml.crypto.Data;
+import java.io.File;
 
 public class Main {
 
     public static void main (String[] args){
-
+    Logger[] loggers = { new DatabaseLogger(), new FileLogger()};
     Customer newCustomer = new Customer();
     newCustomer.setId(1);
     newCustomer.setFirstName("Safa");
     newCustomer.setLastName("Yilmaz");
 
-    CustomerManager customerManager = new CustomerManager(new FileLogger());
+    CustomerManager customerManager = new CustomerManager(loggers);
     customerManager.Add(newCustomer);
 
     }
