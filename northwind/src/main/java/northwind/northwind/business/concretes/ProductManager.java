93e40672687fbaf62548b06d2312def3309fae85
Safa Yilmaz <safa.yilmaz@yahoo.com>
2021-10-06 01:36:31 +0300

    @Service @Autowired

--- northwind/src/main/java/northwind/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/northwind/northwind/business/concretes/ProductManager.java
@@ -3,17 +3,20 @@ package northwind.northwind.business.concretes;
 import northwind.northwind.business.abstracts.ProductService;
 import northwind.northwind.dataAccess.abstracts.ProductDao;
 import northwind.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductManager implements ProductService {
 
     private ProductDao productDao;
 
+    @Autowired
     public ProductManager(ProductDao productDao) {
         this.productDao = productDao;
     }
-
     @Override
     public List<Product> getAll() {
         return null;
